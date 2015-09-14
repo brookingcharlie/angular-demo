a588336a8e241f96071770d139754c2f0d7eb58d
Charles Brooking <charlie@brooking.id.au>
2015-09-15 00:03:20 +1000

    add simple app using ngRoute

---
+++ app/js/app.js
@@ -0,0 +1,14 @@
+'use strict';
+
+var demoApp = angular.module('demoApp', ['ngRoute']);
+
+demoApp.config(['$routeProvider', function($routeProvider) {
+  $routeProvider.when('/', {
+    templateUrl: 'partials/home.html',
+    controller: 'HomeCtrl'
+  })
+}]);
+
+demoApp.controller('HomeCtrl', ['$scope', function($scope) {
+  $scope.value = "foo";
+}]);
